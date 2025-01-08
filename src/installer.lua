c7d5d86b64487875e91d12f4fc009e52fc9bb084
Happygamer1983 <82413775+Happygamer1983@users.noreply.github.com>
2025-01-08 20:25:24 +0000

    Edited installer

--- src/installer.lua
+++ src/installer.lua
@@ -1,17 +1,16 @@
 local args = {...}
 local libraries = {UIFunctions = "https://raw.githubusercontent.com/Happygamer1983/ComputerCraftProjects/refs/heads/main/src/UIFunctions.lua"}
-local TESTlibraries = {UIFunctions = "https://raw.githubusercontent.com/Happygamer1983/ComputerCraftProjects/refs/heads/main/src/UIFunctions.lua"}
+local TESTlibraries = {UIFunctions = "https://raw.githubusercontent.com/Happygamer1983/ComputerCraftProjects/refs/heads/main/src/CCTest.lua"}
 
 fs.makeDir("lib")
 
-if args[1] = "Test" then
+if args[1] == "Test" then
     for LibName, LibURL in pairs(TESTlibraries) do
         local lib = http.get(LibURL)
         local libcode = lib.readAll()
         local libfile = fs.open("lib/" .. LibName, "w")
         libfile.write(libcode)
         libfile.close()
+        require("lib/" .. LibName)
     end
-  
-
 end
