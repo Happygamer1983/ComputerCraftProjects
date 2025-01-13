7ab3c0d61636603a723d633eca5c5ba46748b10a
Happygamer1983 <82413775+Happygamer1983@users.noreply.github.com>
2025-01-13 19:32:35 +0000

    Fixed only working once

--- src/InfoHandler.lua
+++ src/InfoHandler.lua
@@ -1,33 +1,35 @@
 rednet.open("bottom")
 
-local ID, message = rednet.receive()
-print("Message Recieved!")
-
-local CardData = {}
-local HeatCard = peripheral.wrap("left")
-local ReactorCard = peripheral.wrap("right")
-
-local HeatData
-local ReactorData
-
-local success, fail = pcall(function()
-    HeatData = HeatCard.getCardData()
-end)
-
-if not success then
-    HeatData = {"Error getting Heat Card Data!"}
+while true do
+    local ID, message = rednet.receive()
+    print("Message Recieved!")
+    
+    local CardData = {}
+    local HeatCard = peripheral.wrap("left")
+    local ReactorCard = peripheral.wrap("right")
+    
+    local HeatData
+    local ReactorData
+    
+    local success, fail = pcall(function()
+        HeatData = HeatCard.getCardData()
+    end)
+    
+    if not success then
+        HeatData = {"Error getting Heat Card Data!"}
+    end
+    
+    local success, fail = pcall(function()
+        ReactorData = ReactorCard.getCardData()
+    end)
+    
+    if not success then
+        ReactorData = {"Error getting Reactor Card Data!"}
+    end
+    
+    CardData["Heat"] = HeatData
+    CardData["Reactor"] = ReactorData
+    print("Sending Back")
+    rednet.send(ID, textutils.serializeJSON(CardData))
+    sleep()
 end
-
-local success, fail = pcall(function()
-    ReactorData = ReactorCard.getCardData()
-end)
-
-if not success then
-    ReactorData = {"Error getting Reactor Card Data!"}
-end
-
-CardData["Heat"] = HeatData
-CardData["Reactor"] = ReactorData
-print("Sending Back")
-rednet.send(ID, textutils.serializeJSON(CardData))
-
