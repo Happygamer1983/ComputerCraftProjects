17bc7254c66f7c08eff55de3385efc5011a90570
Happygamer1983 <82413775+Happygamer1983@users.noreply.github.com>
2025-01-25 15:48:15 +0000

    Added startup.lua support

--- src/main.lua
+++ src/main.lua
@@ -15,9 +15,9 @@ local CoolantScreens = {}
 local DefaultTextColor = colors.white
 local DefaultBackgroundColor = colors.black
 
-local UIF = require("UIFunctions")
-local Config = require("PerfConfig")
-local Taskmaster = require("taskmaster")()
+local UIF = require("/lib/UIFunctions")
+local Config = require("/lib/PerfConfig")
+local Taskmaster = require("/lib/taskmaster")()
 local expect = require "cc.expect"
 local expect, field = expect.expect, expect.field
 
